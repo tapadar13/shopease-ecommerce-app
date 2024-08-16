91bc80c97b514cf7820dce4b3115fb094efe0836
tapadar13 <monsurismyname@gmail.com>
2024-08-16 09:02:44 +0530

    Integrate Nunito font into application layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,22 +1,38 @@
-import { Inter } from "next/font/google";
+import { Nunito } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import { Providers } from "./providers/Providers";
 import { ClerkProvider } from "@clerk/nextjs";
 import LayoutContent from "./LayoutContent";
 
-const inter = Inter({ subsets: ["latin"] });
+const nunito = Nunito({ subsets: ["latin"] });
 
 export const metadata = {
   title: "ShopEase",
   description: "Your one-stop shop for all your needs",
+  icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    title: "ShopEase",
+    description: "Your one-stop shop for all your needs",
+    images: [
+      {
+        url: "/favicon.ico",
+        width: 800,
+        height: 600,
+        alt: "ShopEase Logo",
+      },
+    ],
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>
+        <body className={nunito.className}>
           <Providers>
             <LayoutContent>{children}</LayoutContent>
             <Toaster />
